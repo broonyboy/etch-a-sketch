6a33b7644553a78ea498f93e810cf6efa9187323
Neale Brown <neale.d.brown@gmail.com>
2019-07-15 20:29:31 +0100

    adding color to grid

---
+++ script.js
@@ -0,0 +1,50 @@
+// Create Grid of divs
+
+const container = document.querySelector('.container');
+const gridArea = document.getElementById('grid');
+let place = "";
+
+
+ 
+
+
+function createGrid(row, column) {
+    let divno = 0;
+    for (let r = 0; r < row; r++) {
+        let rw = document.createElement('div');
+        rw.className = "grid-row";
+        
+        rw.setAttribute('style', 'opacity: 0.3;height: 25px;width: 992px');
+divno++;
+        for (let c = 0; c < column; c++) {
+            let col = document.createElement('div');
+            col.className = 'grid-column';
+            col.setAttribute('id', "cell"+divno);
+            col.setAttribute('onmouseover', "mouseOver(this)");
+            
+            col.setAttribute('style', 'opacity: 0.3;width: 60px;height: 25px');
+            rw.appendChild(col);
+            divno++;
+        }
+        gridArea.appendChild(rw);
+    }
+
+
+    return gridArea;
+}
+
+
+window.onload = createGrid(16, 16);
+
+function mouseOver(elem) {
+    let place = (elem.id);
+    console.log(place);
+    addColor(place);
+return place;
+ }
+function addColor(pixel) {
+    
+   pix = document.getElementById(pixel).classList.add('colorIt');
+   
+
+}
